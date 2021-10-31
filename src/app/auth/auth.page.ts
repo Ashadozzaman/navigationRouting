1c875a07ea64a3cd19db0ba374ec3a1d9c0740a7
Ashadozzaman <shovoua@gmail.com>
2021-10-31 17:54:56 +0600

    input form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -10,6 +11,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
   constructor(
     private authService:AuthService,
     private router:Router,
@@ -32,6 +34,23 @@ export class AuthPage implements OnInit {
           }, 1500);
 
         })
+  }
+
+  onSubmit(form: NgForm){
+    if(!form.valid){
+      return;
+    }
+    const email    =  form.value.email;
+    const password =  form.value.password;
+    console.log(email);
+    if(this.isLogin){
+      //send request to login server
+    }else{
+      //send request to signup server
+    }
+  }
+  onSwitchAuthMode(){
+    this.isLogin = !this.isLogin;
   }
 
 }
